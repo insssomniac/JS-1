f0fa9ab303abb664b1f7ff87d4a01165120b2e15
insssomniac <insssomniac@gmail.com>
2021-03-10 23:38:59 +0200

    work 3 done

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -16,7 +16,22 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
-function isAllTrue(array, fn) {}
+function isAllTrue(array, fn) {
+  if (!(array instanceof Array) || !array.length) {
+    throw new Error('empty array');
+  }
+  if (!(fn instanceof Function)) {
+    throw new Error('fn is not a function');
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (!fn(array[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 /*
  Задание 2:
@@ -34,7 +49,22 @@ function isAllTrue(array, fn) {}
    isSomeTrue([1, 2, 30, 4, 5], n => n > 20) // вернет true
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
-function isSomeTrue(array, fn) {}
+function isSomeTrue(array, fn) {
+  if (!(array instanceof Array) || !array.length) {
+    throw new Error('empty array');
+  }
+  if (!(fn instanceof Function)) {
+    throw new Error('fn is not a function');
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (fn(array[i])) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 /*
  Задание 3:
@@ -47,7 +77,23 @@ function isSomeTrue(array, fn) {}
  3.3: Необходимо выбрасывать исключение в случаях:
    - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn, ...args) {}
+function returnBadArguments(fn, ...args) {
+  if (!(fn instanceof Function)) {
+    throw new Error('fn is not a function');
+  }
+
+  const result = [];
+
+  args.forEach(function (item) {
+    try {
+      fn(item);
+    } catch (e) {
+      result.push(item);
+    }
+  });
+
+  return result;
+}
 
 /*
  Задание 4:
@@ -66,7 +112,49 @@ function returnBadArguments(fn, ...args) {}
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator(number = 0) {}
+function calculator(number = 0) {
+  if (!isFinite(number)) {
+    throw new Error('number is not a number');
+  }
+
+  return {
+    sum: function (...args) {
+      let result = number;
+      for (let i = 0; i < args.length; i++) {
+        result += args[i];
+      }
+      return result;
+    },
+    dif: function (...args) {
+      let result = number;
+      for (let i = 0; i < args.length; i++) {
+        result -= args[i];
+      }
+      return result;
+    },
+    div: function (...args) {
+      let result = number;
+      for (let i = 0; i < args.length; i++) {
+        if (args[i] === 0) {
+          throw new Error('division by 0');
+        }
+        result = result / args[i];
+      }
+      return result;
+    },
+    mul: function (...args) {
+      let result = number;
+      for (let i = 0; i < args.length; i++) {
+        result *= args[i];
+      }
+      return result;
+    },
+  };
+}
+
+const a = calculator(4, 2, 1);
+
+// console.log(a.sum());
 
 /* При решении задач, постарайтесь использовать отладчик */
 
